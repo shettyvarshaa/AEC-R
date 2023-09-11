e4b0f15a1cf5bcb2ec8e2a94d0a6cf00231e6992
shettyvarshaa <shettyvarshaa275@gmail.com>
2023-09-11 14:09:53 +0530

    Added 4th program

---
+++ #4.R
@@ -0,0 +1,25 @@
+#string = "Hello World"
+string = readline("Enter the String: ")
+print(string)
+# nchar function
+nchar(string) 
+# toupper function
+toupper(string) 
+# tolower function
+tolower(string) 
+# substr function
+substr(string, 5, 20) 
+# grep function
+grep("or", string) 
+# paste function
+paste("hello", "world", string, sep = "-") 
+# strsplit function
+strsplit(string,'el') 
+# sprintf function
+age = 40L
+name = "Ram"
+sprintf("%s is %d years old", name, age) 
+# cat function
+cat("hello","world",string,sep = "-") 
+# sub function
+sub("World", "there!", string)
