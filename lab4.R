4a3cc61b8bdc3d97638ccbcf58ae3b1b28ea7be0
shettyvarshaa <shettyvarshaa275@gmail.com>
2023-09-11 14:38:16 +0530

    Optimized 4 and 3

--- lab4.R
+++ lab4.R
@@ -1,25 +1,25 @@
-#string = "Hello World"
 string = readline("Enter the String: ")
 print(string)
-# nchar function
+
 nchar(string) 
-# toupper function
+
 toupper(string) 
-# tolower function
+
 tolower(string) 
-# substr function
-substr(string, 5, 20) 
-# grep function
-grep("or", string) 
-# paste function
+
+substr(string, 5, 10) 
+
+grep("Peo", string) 
+
 paste("hello", "world", string, sep = "-") 
-# strsplit function
-strsplit(string,'el') 
-# sprintf function
+
+strsplit(string,'ey') 
+
 age = 40L
 name = "Ram"
 sprintf("%s is %d years old", name, age) 
-# cat function
+
 cat("hello","world",string,sep = "-") 
-# sub function
-sub("World", "there!", string)
+
+sub("People", "there!", string)
+
