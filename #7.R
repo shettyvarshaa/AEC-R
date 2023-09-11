2ef4e2ec266c30e8f55127f207a4fcd7365de06d
shettyvarshaa <shettyvarshaa275@gmail.com>
2023-09-11 12:10:31 +0530

    Added 7 again

--- #7.R
+++ #7.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(ggplot2) 
 data(iris) 
 
@@ -22,30 +21,3 @@ scatter_plot = ggplot(iris, aes(x = Petal.Length, y = Petal.Width, color =Specie
   scale_shape_manual(values = c("setosa" = 16, "versicolor" = 17, "virginica"= 18)) 
 print(scatter_plot)
 
-
-=======
-library(ggplot2) 
-data(iris) 
-
-# Bar Plot
-bar_plot = ggplot(iris, aes(x = Species)) + 
-  geom_bar(fill="lightblue") + 
-  labs(title = "Bar Plot of Iris Species", x = "Species", y = "Count") 
-print(bar_plot) 
-
-# Line Plot
-line_plot = ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width, color =Species)) + 
-  geom_line() + 
-  labs(title = "Line Plot of Sepal Length vs Sepal Width", x ="Sepal Length", y = "Sepal Width") 
-print(line_plot) 
-
-# Scatter Plot
-scatter_plot = ggplot(iris, aes(x = Petal.Length, y = Petal.Width, color =Species, shape = Species)) + 
-  geom_point(size = 3, alpha =0.7) + 
-  labs(title = "Scatter Plot of Petal Length vsPetal Width", x = "Petal Length", y = "Petal Width") +
-  scale_color_manual(values = c("setosa" = "blue", "versicolor" = "green", "virginica" = "red")) + 
-  scale_shape_manual(values = c("setosa" = 16, "versicolor" = 17, "virginica"= 18)) 
-print(scatter_plot)
-
-
->>>>>>> 39ba72ef184572a06105132e44075bdac98ada2a
